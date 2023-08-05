ce33c7135c30902a83ebca45d4400346fc9090d8
r567tw <r567tw@gmail.com>
2023-08-05 17:24:32 +0800

    Fetching and Outputting Orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -100,11 +100,12 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders
+  req.user.getOrders({ include: ["products"] })
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
-        pageTitle: 'Your Orders'
+        pageTitle: 'Your Orders',
+        orders: orders
       });
     })
 
