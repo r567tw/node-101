f8868d393cdab48b7a0d8ce9b1661182576c87de
r567tw <r567tw@gmail.com>
2023-08-05 17:15:57 +0800

    empty carts

--- controllers/shop.js
+++ controllers/shop.js
@@ -100,15 +100,21 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user.getOrders
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
+      });
+    })
+
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart
   req.user.getCart()
     .then((cart) => {
+      fetchedCart = cart
       return cart.getProducts();
     })
     .then((products) => {
@@ -120,6 +126,8 @@ exports.postOrder = (req, res, next) => {
             return product;
           }))
         }).catch(err => console.log(err))
+    }).then(result => {
+      return fetchedCart.setProducts(null)
     }).then(result => {
       res.redirect("/orders")
     }).catch(err => console.log(err))
